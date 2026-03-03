9c1151fb6019c987adb64bd9d46daed9a82c2991
miSSkSambo <katlego2390@gmail.com>
2026-03-03 17:05:22 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@db:5432/pyblazex"
+
+settings = Settings()
